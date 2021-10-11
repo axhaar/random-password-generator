7e2983af0f54eced1d2b9b6f418812310caaea72
axhaar <pranshumishra2001@gmail.com>
2021-10-11 16:21:04 +0530

    func generator start

---
+++ password.py
@@ -0,0 +1,12 @@
+import tkinter
+import pyperclip
+import random
+import string
+
+pass_str = StringVar()
+
+def Generator():
+    password = ''
+    for x in range (0,4):
+        password = random.choice(string.ascii_uppercase)+random.choice(string.ascii_lowercase)+random.choice(string.digits)+random.choice(string.punctuation)
+    
