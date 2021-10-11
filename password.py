9dfde5d4b92e88eaebd056f97bdc23f9146a98aa
axhaar <pranshumishra2001@gmail.com>
2021-10-11 16:22:22 +0530

    defining Generator()

--- password.py
+++ password.py
@@ -9,4 +9,6 @@ def Generator():
     password = ''
     for x in range (0,4):
         password = random.choice(string.ascii_uppercase)+random.choice(string.ascii_lowercase)+random.choice(string.digits)+random.choice(string.punctuation)
-    
+    for y in range(pass_len.get()- 4):
+        password = password+random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits + string.punctuation)
+    pass_str.set(password)
