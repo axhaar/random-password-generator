6d9d8643c4064ccf2b0655e468a6e54cd10a1ce9
axhaar <pranshumishra2001@gmail.com>
2021-10-11 16:31:32 +0530

    select password length

--- password.py
+++ password.py
@@ -11,3 +11,7 @@ root.title("axhaar - PASSWORD GENERATOR")
 Label(root, text = 'PASSWORD GENERATOR' , font ='arial 15 bold').pack()
 Label(root, text ='axhaar ©', font ='arial 15 bold').pack(side = BOTTOM)
 
+pass_label = Label(root, text = 'PASSWORD LENGTH', font = 'arial 10 bold').pack()
+pass_len = IntVar()
+length = Spinbox(root, from_ = 8, to_ = 32 , textvariable = pass_len , width = 15).pack()
+
