30181593c9da15f247fd187e62a087b8f9e70fc7
axhaar <pranshumishra2001@gmail.com>
2021-10-11 17:39:07 +0530

    Function to generate password

--- password.py
+++ password.py
@@ -15,3 +15,11 @@ pass_label = Label(root, text = 'PASSWORD LENGTH', font = 'arial 10 bold').pack(
 pass_len = IntVar()
 length = Spinbox(root, from_ = 8, to_ = 32 , textvariable = pass_len , width = 15).pack()
 
+pass_str = StringVar()
+def Generator():
+    password = ''
+    for x in range (0,4):
+        password = random.choice(string.ascii_uppercase) + random.choice(string.ascii_lowercase) + random.choice(string.digits) + random.choice(string.punctuation)
+    for y in range(pass_len.get()- 4):
+        password = password + random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits + string.punctuation)
+    pass_str.set(password)
