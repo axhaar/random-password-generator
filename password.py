88461ea4aeca6ffef138b8b21943a6305eee7ecf
axhaar <pranshumishra2001@gmail.com>
2021-10-14 20:20:20 +0530

    minor adjustments to radiobuttons

--- password.py
+++ password.py
@@ -8,7 +8,7 @@ import string
 
 #initialize window
 root =Tk()
-root.geometry("400x400")
+root.geometry("400x300")
 root.resizable(10,10)
 root.title("axhaar - PASSWORD GENERATOR")
 var = IntVar()
@@ -16,26 +16,26 @@ var1 = IntVar()
 
 #heading
 Label(root, text = 'PASSWORD GENERATOR' , font ='arial 15 bold').pack()
-Label(root, text ='axhaar ©', font ='calibri 11').pack(side = BOTTOM)
+Label(root, text ='axhaar ©', font ='arial 11').pack(side = BOTTOM)
 
 #password length
 pass_label = Label(root, text = 'PASSWORD LENGTH', font = 'arial 10 bold').pack()
 pass_len = IntVar()
-length = Spinbox(root, from_ = 8, to_ = 32 , textvariable = pass_len , width = 15).pack()
+length = Spinbox(root, from_ = 8, to_ = 32 , textvariable = pass_len , width = 16).pack()
 
 #define function
 pass_str = StringVar()
 def Generator():
     password = ''
-    if var.get() == 0:
+    if var.get() == 1:
         for x in range (0,4):
             password = random.choice(string.ascii_lowercase)
         for y in range(pass_len.get()- 4):
             password = password + random.choice(string.ascii_lowercase + string.digits)
 
-    elif var.get() == 1:
+    elif var.get() == 0:
         for x in range (0,4):
-            password = random.choice(string.ascii_uppercase) + random.choice(string.ascii_lowercase)
+            password = random.choice(string.ascii_uppercase) + random.choice(string.digits)
         for y in range(pass_len.get()- 4):
             password = password + random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) 
 
@@ -47,9 +47,11 @@ def Generator():
 
     pass_str.set(password)
 
-radio_low = Radiobutton(root, text="Low", variable=var, value=0)
+#radiobuttons for password strength
+radio_low = Radiobutton(root, text="Low", variable=var, value=1)
 radio_low.pack(anchor=CENTER)
-radio_middle = Radiobutton(root, text="Medium", variable=var, value=1)
+#value=0 means the default will be fixed to "Medium"
+radio_middle = Radiobutton(root, text="Medium", variable=var, value=0)
 radio_middle.pack(anchor=CENTER)
 radio_strong = Radiobutton(root, text="Strong", variable=var, value=3)
 radio_strong.pack(anchor=CENTER)
